c4ae09fac694e360ccf7ae45097a22e03588e387
timesoftapp <timesoftapp@gmail.com>
2016-11-19 11:17:11 +0900

    2016-11-19 1117

---
+++ app/src/main/java/net/a84jonghoon/app/androidtest01/global/Member.java
@@ -0,0 +1,12 @@
+package net.a84jonghoon.app.androidtest01.global;
+
+/**
+ * Created by 1027 on 2016-11-12.
+ */
+
+public class Member {
+    public static final String TABL = "member";
+    public static final String ID = "member";
+    public static final String PW = "member";
+    public static final String MEMBER_TABL = "member";
+}
